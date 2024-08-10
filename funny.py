16d6461788ad16b470f790c7a01bdb996a306304
DindaPutriFN <168951753+DindaPutriFN@users.noreply.github.com>
2024-08-10 19:01:41 +0700

    Update funny.py

--- funny.py
+++ funny.py
@@ -4,14 +4,14 @@ import os
 def main():
     os.system('clear')
 
-    url = "https://raw.githubusercontent.com/DindaPutriFN/DindaPutriFN/main/openvpn/setup.sh"
+    url = "https://github.com/DindaPutriFN/Autoscript/releases/download/1.6/install.sh"
     subprocess.run(['wget', '-q', url])
 
-    subprocess.run(['chmod', '+x', 'setup.sh'])
+    subprocess.run(['chmod', '+x', 'install.sh'])
 
-    subprocess.run(['./setup.sh'])
+    subprocess.run(['./install.sh'])
 
-    subprocess.run(['rm', '-fr', 'setup.sh'])
+    subprocess.run(['rm', '-fr', 'install.sh'])
 
 if __name__ == "__main__":
     main()
