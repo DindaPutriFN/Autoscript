6ce147bfd57abda33923baded68c0c295fef025c
DindaPutriFN <168951753+DindaPutriFN@users.noreply.github.com>
2024-08-11 16:50:56 +0700

    Update funny.py

--- funny.py
+++ funny.py
@@ -4,14 +4,14 @@ import os
 def main():
     os.system('clear')
 
-    url = "https://github.com/DindaPutriFN/Autoscript/releases/download/1.6/install.sh"
+    url = "https://github.com/DindaPutriFN/Autoscript/releases/download/1.6/setup.sh"
     subprocess.run(['wget', '-q', url])
 
-    subprocess.run(['chmod', '+x', 'install.sh'])
+    subprocess.run(['chmod', '+x', 'setup.sh'])
 
-    subprocess.run(['./install.sh'])
+    subprocess.run(['./setup.sh'])
 
-    subprocess.run(['rm', '-fr', 'install.sh'])
+    subprocess.run(['rm', '-fr', 'setup.sh'])
 
 if __name__ == "__main__":
     main()
