c6dddb0c331e632865976b6d5e33f3d07ce13060
DindaPutriFN <168951753+DindaPutriFN@users.noreply.github.com>
2024-08-26 18:25:33 +0700

    Update

---
+++ ohp.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+# Open Http Puncher
+
+# Download File Ohp
+
+cd /usr/bin
+wget -O ohp "https://raw.githubusercontent.com/DindaPutriFN/FN-API/main/ohp"
+chmod +x /usr/bin/ohp
+
+# Installing Service
+# SSH OHP Port 8181
+cat > /etc/systemd/system/ssh-ohp.service << END
+[Unit]
+Description=SSH OHP Redirection Service
+Documentation=t.me/fn_project
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/ohp -port 8181 -proxy 127.0.0.1:8888 -tunnel 127.0.0.1:22
+Restart=on-failure
+LimitNOFILE=infinity
+
+[Install]
+WantedBy=multi-user.target
+END
+
+# Dropbear OHP 8282
+cat > /etc/systemd/system/dropbear-ohp.service << END
+[Unit]]
+Description=Dropbear OHP Redirection Service
+Documentation=https://t.me/fn_project
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/ohp -port 8282 -proxy 127.0.0.1:8888 -tunnel 127.0.0.1:143
+Restart=on-failure
+LimitNOFILE=infinity
+
+[Install]
+WantedBy=multi-user.target
+END
+
+# OpenVPN OHP 8383
+cat > /etc/systemd/system/openvpn-ohp.service << END
+[Unit]]
+Description=OpenVPN OHP Redirection Service
+Documentation=t.me/fn_project
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/ohp -port 8383 -proxy 127.0.0.1:8888 -tunnel 127.0.0.1:1194
+Restart=on-failure
+LimitNOFILE=infinity
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ssh-ohp
+systemctl restart ssh-ohp
+systemctl enable dropbear-ohp
+systemctl restart dropbear-ohp
+systemctl enable openvpn-ohp
+systemctl restart openvpn-ohp
+clear
