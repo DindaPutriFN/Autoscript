2e4352a502446e777f5d8a40a565683ca0cdb361
DindaPutriFN <168951753+DindaPutriFN@users.noreply.github.com>
2024-09-01 18:18:02 +0700

    Update funny.py

--- funny.py
+++ funny.py
@@ -4,7 +4,7 @@ import os
 def main():
     os.system('clear')
 
-    url = "https://github.com/DindaPutriFN/Autoscript/releases/download/1.8/setup-acc.sh"
+    url = "https://github.com/DindaPutriFN/Autoscript/releases/download/1.9/setup.sh"
     subprocess.run(['wget', '-O', 'setup.sh', '-q', url])
 
     subprocess.run(['chmod', '+x', 'setup.sh'])
